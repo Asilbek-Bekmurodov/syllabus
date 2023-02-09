55f9fe1375f5ad75e336d2c97eafd7f4cc8a62e1
Asilbek2002 <school2924@gmail.com>
2023-02-10 00:49:00 +0500

    feat: navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
+import Logo from "../../assets/images/logo.jpg";
 import cls from "./navbar.module.scss";
+
+const links = [
+  "Universitet",
+  "Ommabop sillabuslar",
+  "Statistika",
+  "Universitetlar reytingi",
+  "Biz haqimizda",
+];
 
 export class Navbar extends Component {
   render() {
-    return <div>Navbar</div>;
+    return (
+      <div className={cls.wrapper}>
+        <div className={cls.navbar}>
+          <div className={cls.logo}>
+            <img src={Logo} alt="" />
+          </div>
+          <div className={cls.search}>
+            <input type="text" placeholder="Qidiruv" />
+          </div>
+          <div className={cls.last}>
+            <div className={cls.language}>O'zbekcha</div>
+            <button className={cls.login}>Kirish</button>
+          </div>
+        </div>
+        <div className={cls.links}>
+          <ul className={cls.items}>
+            {links.map((link) => (
+              <li className={cls.item}>
+                <a className={cls.link} href="#1">
+                  {link}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import Navbar from "../../components/navbar";
 import cls from "./home.module.scss";
 
 export class Home extends Component {
   render() {
-    return <div className={cls.wrapper}>Home</div>;
+    return (
+      <div className={cls.wrapper}>
+        <Navbar />
+      </div>
+    );
   }
 }
 
