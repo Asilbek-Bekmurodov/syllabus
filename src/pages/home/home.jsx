82cebe311a01e8d12ae8b088d0334daf75db36ef
Asilbek2002 <school2924@gmail.com>
2023-02-15 15:14:47 +0500

    feat: news section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,6 +3,8 @@ import RightArrow from "../../assets/icons/right-arrow";
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
 import cls from "./home.module.scss";
+import { MdCalendarToday } from "react-icons/md";
+import { FaEye } from "react-icons/fa";
 
 const cards = [
   {
@@ -20,6 +22,28 @@ const cards = [
   {
     color: "#C7074F",
     title: "Koronovirus info",
+  },
+];
+
+const news = [
+  {
+    imgURL:
+      "https://daryo.uz/static/2023/zulfiya/photo_2023-02-13_21-54-39.jpg",
+    title: "Turkiyadagi shahar zilzila sababli butunlay buzib tashlanadi",
+  },
+  {
+    imgURL: "https://daryo.uz/static/2023/02/medium-63ebc5a0cb190.jpg",
+    title:
+      "“Daryo kundaligi”da 14-fevral kuni sodir bo‘lgan eng muhim voqealar ",
+  },
+  {
+    imgURL: "https://daryo.uz/static/Fw5ofP15784890544366_l.jpg",
+    title:
+      "“Menga nisbatan bosim yoki qiynoq boʻlmadi” – tovlamachilikda gumon qilinib, qamoqqa olingan jurnalist Xurshid Daliyev tergov qanday olib borilayotgani yuzasidan izoh berdi",
+  },
+  {
+    imgURL: "https://daryo.uz/static/2023/02/thumb-63ebafda57250.jpg",
+    title: "O‘zbekiston banklarida kreditlar ajratish ko‘paymoqda",
   },
 ];
 export class Home extends Component {
@@ -124,6 +148,24 @@ export class Home extends Component {
 
           <div className={cls.news}>
             <h4>Dolzarb xabarlar</h4>
+            <div className={cls.container}>
+              {news.map(({ imgURL, title }) => (
+                <div className={cls.new}>
+                  <img src={imgURL} alt="" />
+                  <p className={cls.paragraph}>{title}</p>
+                  <div className={cls["bottom-info"]}>
+                    <p>
+                      <MdCalendarToday />
+                      10:39 | 22.01.2021
+                    </p>
+                    <p>
+                      <FaEye />
+                      1066
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </section>
         <Footer />
