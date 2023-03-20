7a6f868655c3e0070539607b3947f1c97cfc26c1
zoirovulmas <zoirov.ulmas@gmail.com>
2023-03-20 19:00:59 +0500

    co

--- src/pages/dashboard/routeData.js
+++ src/pages/dashboard/routeData.js
@@ -9,14 +9,14 @@ import Test from "./contents/java/Test";
 import Video from "./contents/java/video";
 
 export const routeData = [
-  { content: <AboutCourse/>, path: "about-course-java" },
-  { content: <Structure/>, path: "structure-java" },
-  { content: <Assessment/>, path: "baxolash-java" },
-  { content: <Test/>, path: "test-java" },
-  { content: <Independent/>, path: "mustaqil-ish-java" },
-  { content: <Internet/>, path: "internet-resusrlar-java" },
-  { content: <Literature/>, path: "adabiyotlar-java" },
-  { content: <Video />, path: "video-java" },
-  { content: <Lecture/>, path: "maruza-java" },
+  { content: < AboutCourse />, path: "about-course-java" },
+  { content: < Structure />, path: "structure-java" },
+  { content: < Assessment />, path: "baxolash-java" },
+  { content: < Test />, path: "test-java" },
+  { content: < Independent />, path: "mustaqil-ish-java" },
+  { content: < Internet />, path: "internet-resusrlar-java" },
+  { content: < Literature />, path: "adabiyotlar-java" },
+  { content: < Video />, path: "video-java" },
+  { content: < Lecture />, path: "maruza-java" },
 
 ];
