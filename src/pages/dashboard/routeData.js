93110213dc5f44ea61e2adb67fdbb84c02e9b445
zoirovulmas <zoirov.ulmas@gmail.com>
2023-03-22 11:07:23 +0500

    “commit124”

--- src/pages/dashboard/routeData.js
+++ src/pages/dashboard/routeData.js
@@ -1,15 +1,15 @@
 import AboutCourse from "./contents/java/about-course";
 import Assessment from "./contents/java/assessment";
-import Independent from "./contents/java/Independent";
+import Independent from "./contents/java/independent";
 import Internet from "./contents/java/internet";
 import Lecture from "./contents/java/lecture";
 import Literature from "./contents/java/literature";
 import Structure from "./contents/java/stucture";
-import Test from "./contents/java/Test";
+import Test from "./contents/java/test";
 import Video from "./contents/java/video";
 
 export const routeData = [
-  { content: < AboutCourse/>, path: "about-course-java" },
+  { content: < AboutCourse />, path: "about-course-java" },
   { content: < Structure />, path: "structure-java" },
   { content: < Assessment />, path: "baxolash-java" },
   { content: < Test />, path: "test-java" },
