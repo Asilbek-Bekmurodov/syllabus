a100fdf5ee88f2dae2e16ae37fb14f78b1428059
zoirovulmas <zoirov.ulmas@gmail.com>
2023-03-26 06:56:09 +0500

    javaindepent

--- src/pages/dashboard/contents/java/independent.jsx
+++ src/pages/dashboard/contents/java/independent.jsx
@@ -6,7 +6,7 @@ function IndependentJava () {
       <h1 className={cls.title}>Independent  section </h1>
       <p className={cls.paragraph}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quam
-          Independent section
+          Independent sectionjjjjjjj
       </p>
     </div>
   );
