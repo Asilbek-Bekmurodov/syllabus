dabbd329e5fc860703ecde51a5080a7fa5724636
zoirovulmas <zoirov.ulmas@gmail.com>
2023-03-27 17:08:57 +0500

    “messag782”

--- src/pages/dashboard/contents/java/about-course.jsx
+++ src/pages/dashboard/contents/java/about-course.jsx
@@ -7,40 +7,7 @@ function AboutCourseJava() {
   return (
     <div>
       <h1 className={cls.title}>About course</h1>
-      <table style="border-collapse: collapse; width: 99.9449%;" border="1">
-<tbody>
-<tr>
-<td style="width: 16.6851%;"><strong>Ma'ruza</strong></td>
-<td style="width: 16.6851%;">dasd</td>
-<td style="width: 16.6851%;">das</td>
-<td style="width: 16.6851%;">123</td>
-<td style="width: 16.6851%;">&nbsp;</td>
-<td style="width: 8.27358%;">&nbsp;</td>
-<td style="width: 4.13679%;">&nbsp;</td>
-<td style="width: 4.13679%;">&nbsp;</td>
-</tr>
-<tr>
-<td style="width: 16.6851%;">32</td>
-<td style="width: 16.6851%;">123</td>
-<td style="width: 16.6851%;">123</td>
-<td style="width: 16.6851%;">dsadas</td>
-<td style="width: 16.6851%;">&nbsp;</td>
-<td style="width: 8.27358%;">&nbsp;</td>
-<td style="width: 4.13679%;">&nbsp;</td>
-<td style="width: 4.13679%;">&nbsp;</td>
-</tr>
-<tr>
-<td style="width: 16.6851%;">&nbsp;</td>
-<td style="width: 16.6851%;">&nbsp;</td>
-<td style="width: 16.6851%;">&nbsp;</td>
-<td style="width: 16.6851%;">&nbsp;</td>
-<td style="width: 16.6851%;">&nbsp;</td>
-<td style="width: 8.27358%;">&nbsp;</td>
-<td style="width: 4.13679%;">&nbsp;</td>
-<td style="width: 4.13679%;">&nbsp;</td>
-</tr>
-</tbody>
-</table>
+ 
     </div>
   );
 }
