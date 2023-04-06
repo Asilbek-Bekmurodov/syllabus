303efd5b568fc0c940d1884a592c5420e71f8d1e
zoirovulmas <zoirov.ulmas@gmail.com>
2023-04-06 11:51:06 +0500

    “change40”

--- src/components/photo-carusel/inside.js
+++ src/components/photo-carusel/inside.js
@@ -3,5 +3,5 @@ export const Photos = [
   "https://static.tuit.uz/uploads/1/W73eM8T-hn5cLRoa_rQWKshn3eUutXvm.png",
   "https://upload.wikimedia.org/wikipedia/commons/4/4e/National_University_of_Uzbekistan_Logo.png",
   "https://yt3.googleusercontent.com/ytc/AL5GRJXEHF-81g_tKFg5ajTIfPU6IfidKplRUkcKEfVh=s900-c-k-c0x00ffffff-no-rj",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUr_TIPT1fQoWBaM4vSxmkf2noV04VEpaQxOrl0n7BImrcQghfCi3JN0YrSyXY8ssxUnQ&usqp=CAU",
+  "https://en.wikipedia.org/wiki/Tashkent_State_University_of_Economics#/media/File:Tsue_logo_en.png",
 ];
